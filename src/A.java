aab004631393cebe5a1cda3809f03907343303a5
Manuel Galbier <manuel.galbier@gmail.com>
2016-05-17 12:49:37 +0200

    initial commit

---
+++ src/A.java
@@ -0,0 +1,12 @@
+
+
+public class A {
+	
+	public int a()
+	{
+		int x = 1;
+		int y = 2;
+		return x+y;
+	}
+
+}
---
+++ src/B.java
@@ -0,0 +1,12 @@
+
+
+public class B {
+	int x = 2;
+	int y = 2;
+	
+	public int b()
+	{
+		return this.x * this.y;
+	}
+
+}
